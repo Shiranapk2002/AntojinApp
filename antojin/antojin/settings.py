55348f4857b556f6644f2af3bcfff24df422696b
Shiranapk2002 <shiranapk@gmail.com>
2023-11-15 23:48:18 +0530

    https://github.com/Shiranapk2002/AntojinApp.git

--- antojin/antojin/settings.py
+++ antojin/antojin/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'antojin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'antojindb',
         'USER':'root',
         'PASSWORD':'Shirana@123',
--- antojin/app3/urls.py
+++ antojin/app3/urls.py
@@ -9,5 +9,4 @@ from .views import DressList, DressDetailView
 urlpatterns = [
     path('',DressList.as_view(), name='dresslist'),
     path('detail/int:<pk>/',DressDetailView.as_view(),name='detail_view'),
-    path()
 ]
